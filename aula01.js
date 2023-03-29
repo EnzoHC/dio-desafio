65a13131144f5034d7efe3237061b13654f8c022
EnzoHC <102993223+EnzoHC@users.noreply.github.com>
2023-03-28 21:30:35 -0300

    Delete aula01.js

--- aula01.js
+++
@@ -1,7 +0,0 @@
-const precoGasolina = 4.50;
-const gastoMedio = 10;
-const distancia = 120;
-
-const litrosConsumidos = distancia / gastoMedio;
-const precoViagem = litrosConsumidos * precoGasolina;
-console.log(precoViagem.toFixed(2));
